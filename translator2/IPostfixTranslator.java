8df2c036b55aead33c87a30069d0a566ee7771b9
Ryan <user@Ryans-MacBook-Pro.local>
2024-11-16 19:56:31 +0100

    sprint two

---
+++ translator2/IPostfixTranslator.java
@@ -0,0 +1,8 @@
+package translator2;
+
+public interface IPostfixTranslator {
+    
+    public String translate() throws Exception;
+    public int evaluate() throws Exception;
+
+}
--- translator2/PostfixTranslator.java
+++ translator2/PostfixTranslator.java
@@ -1,4 +1,6 @@
-package translator1;
+package translator2;
+
+import java.util.Stack;
 
 import slu.compiler.*;
 
@@ -48,6 +50,7 @@ public class PostfixTranslator implements IPostfixTranslator {
     private IToken token;
     private IScanner scanner;
     private String postfix;
+    private Stack<Integer> evaluator = new Stack<>();
     
     public PostfixTranslator(IScanner lex) {
         this.scanner = lex;
@@ -157,5 +160,83 @@ public class PostfixTranslator implements IPostfixTranslator {
             throw new Exception("\nError at line " + this.scanner.getLine() + ": " + (this.scanner.getLexeme(tokenName).equals("null") ? "token " + tokenName +
                                 " is not defined in 'lexicon.txt'" : tokenName + " expected"));
         }
+    }
+
+    // Boolean function to check if the character is an operator
+    private boolean isOperator(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%';
+    }
+
+    // Return the result of postfix operation
+    // Two numbers get popped from stack
+    // Operator being detected from string triggers the function
+    private int operate(int num1, int num2, char operator) throws Exception {
+        switch (operator) {
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+            case '*': return num1 * num2;
+            case '/': if (num2 == 0) throw new Exception("Division by zero");
+                      return num1 / num2;
+            case '%': return num1 % num2;
+        default: throw new Exception("Unsupported operator: " + operator);
+        }
+    }
+
+    // if character is an operator --> pop stack
+    // operate with stack content and operator
+    // push result to stack
+
+    public int evaluate() throws Exception {
+
+        System.out.println("postfix string: " + this.postfix);
+        // Iterator to go through postfix
+        int i = 0;
+
+        while(i < this.postfix.length()) {
+            // Save current character
+            char ch = this.postfix.charAt(i);
+
+            // Move to next character if it is a space
+            if (ch == ' ') {
+                i++;
+                continue;
+            }
+
+            // If it is a digit --> take into account potential numbers with multiple digits
+            else if (Character.isDigit(ch)) {
+                int num = 0;
+
+                while(i < this.postfix.length() && Character.isDigit(this.postfix.charAt(i))) {
+
+                    // Multiply current value by 10 and add new character converted to an int
+                    // How to convert a char to an int: charAt(i) - '0'
+                    num = (num * 10) + (this.postfix.charAt(i) - '0');
+
+                    // Increment i to see if next character is also an int
+                    i++;
+                }
+                // Push into stack and move onto next character in postfix string
+                System.out.println("Pushing number into stack: " + num);
+                evaluator.push(num);
+            }
+
+            else if (isOperator(ch)) {
+                System.out.println("Operator trigger: " + ch);
+                int num2 = evaluator.pop();
+                int num1 = evaluator.pop();
+
+                int result = operate(num1, num2, ch);
+                System.out.println("Result into stack: " + result);
+                evaluator.push(result);
+
+                i++;
+            }
+
+            else {
+                throw new Exception("Invalid character in expression: " + ch);
+            }
+        }
+        // Return the last int in the stack
+        return evaluator.pop();
     }
 }
